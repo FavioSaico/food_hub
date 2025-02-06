17d97f676482a7c84b0a55a8e7c204ce94184993
RosaEstrada <rosamercedes.estrada@unmsm.edu.pe>
2025-02-05 19:51:20 -0500

    Backup de cambios en main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,22 +3,16 @@ import 'package:food_hub/pages/auth/login_page.dart';
 import 'package:food_hub/providers/auth_provider.dart';
 import 'package:food_hub/providers/food_provider.dart';
 import 'package:food_hub/pages/auth/registro_usuario_page.dart';
-<<<<<<< HEAD
-import 'package:food_hub/pages/reserva/detalle.dart';
-import 'package:food_hub/pages/reserva/sedes.dart';
-=======
 
 import 'package:food_hub/pages/home/main_food_page.dart';
 import 'package:food_hub/pages/food/RegisterCard.dart';
 import 'package:food_hub/pages/home/Inicio1.dart';
 import 'package:food_hub/pages/home/Inicio2.dart';
 
->>>>>>> 1decbdf82f84368d526c7d372075bafeca901a81
 import 'package:food_hub/pages/user/admin_profile_page.dart';
 import 'package:food_hub/pages/user/admin_view_page.dart';
 import 'package:food_hub/pages/user/user_profile_page.dart';
 import 'package:get/get.dart';
-
 
 import 'package:provider/provider.dart';
 
@@ -34,8 +28,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (_) => FoodProvider() ),
-        ChangeNotifierProvider(create: (_) => AuthProvider() ),
+        ChangeNotifierProvider(create: (_) => FoodProvider()),
+        ChangeNotifierProvider(create: (_) => AuthProvider()),
       ],
       child: GetMaterialApp(
         debugShowCheckedModeBanner: false,
@@ -49,8 +43,8 @@ class MyApp extends StatelessWidget {
           '/adminProfile': (context) => PerfilAdminPage(),
           '/adminView': (context) => VistaAdminPage(),
           '/register-card': (context) => const RegisterCard(),
-          '/iniciolog':(context) => const SplashScreen(),
-          '/iniciolog2':(context) => const SplashScreen2(),
+          '/iniciolog': (context) => const SplashScreen(),
+          '/iniciolog2': (context) => const SplashScreen2(),
         },
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
